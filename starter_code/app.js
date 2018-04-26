a3c625a9bee3c8192bb91201e0a7bccad4b3da4a
Luis Flores <leinad15.df@gmail.com>
2018-04-25 20:24:17 -0400

    Page functions started styling the home page

--- starter_code/app.js
+++ starter_code/app.js
@@ -6,12 +6,37 @@ const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
 
+// always goes first!!!!
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    // console.log(beers);
+    res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/random-beers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    // console.log(beers);
+    res.render('randomBeer', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
